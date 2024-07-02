e4667ca9b7de9ecb690f6dfa45f4be469fba8be0
Salvador Solis <salvador920110@gmail.com>
2024-07-02 17:42:25 -0600

    cambio en tabla perfiles

--- foro/src/main/resources/db/migration/V3__create-table-usuarios.sql
+++ foro/src/main/resources/db/migration/V3__create-table-usuarios.sql
@@ -1,9 +1,8 @@
 create table usuarios(
     id int not null auto_increment,
     nombre varchar(150) not null,
-    correo_electronico varchar(150) not null,
+    correo_electronico varchar(150) not null unique,
     contrasenia varchar(150) not null,
-    perfiles varchar(150) not null,
 
     primary key(id)
 );
--- foro/src/main/resources/db/migration/V9__insert-table-usuarios.sql
+++ foro/src/main/resources/db/migration/V9__insert-table-usuarios.sql
@@ -1,3 +1,3 @@
 INSERT INTO foro_alura.usuarios
-(id, nombre, correo_electronico, contrasenia, perfiles)
-VALUES(null, 'salvador solis', 'salvador@correo.com', '123456', '1');
+(id, nombre, correo_electronico, contrasenia)
+VALUES(null, 'salvador solis', 'salvador@correo.com', '123456');
