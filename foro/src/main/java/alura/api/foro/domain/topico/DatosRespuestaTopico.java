14067a5d2a4b8277a47ec9919b8bbcf7b437cc3f
Salvador Solis <salvador920110@gmail.com>
2024-07-02 12:09:05 -0600

    formato de fecha a dia-mes-año

--- foro/src/main/java/alura/api/foro/domain/topico/DatosRespuestaTopico.java
+++ foro/src/main/java/alura/api/foro/domain/topico/DatosRespuestaTopico.java
@@ -1,12 +1,14 @@
 package alura.api.foro.domain.topico;
 
 import alura.api.foro.domain.autor.Autor;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
 public record DatosRespuestaTopico(
         String titulo,
         String mensaje,
+        @JsonFormat(pattern = "dd-MM-yyyy")
         LocalDateTime fechaCreacion,
         Boolean status,
         String autor,
