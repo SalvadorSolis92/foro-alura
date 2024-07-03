7b25d03cb9600d397bb6f3831fbf73f0b9ab4870
Salvador Solis <salvador920110@gmail.com>
2024-07-02 18:24:13 -0600

    quitando imports

--- foro/src/main/java/alura/api/foro/controller/TopicoController.java
+++ foro/src/main/java/alura/api/foro/controller/TopicoController.java
@@ -13,8 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class TopicoController {
 
